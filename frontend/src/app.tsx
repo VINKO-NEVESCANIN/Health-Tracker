24b694fa2d465ac580352737c0a9ac6c6de7d1aa
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2025-09-10 18:45:30 -0700

    CambioEnScreens.
    Modificación de ruta de las pestanas de la aplicación.

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./src/screens/HomeScreen";
-import RegistroScreen from "./src/screens/RegistroScreen";
-import DoctorScreen from "./src/screens/DoctorScreen";
-import PacienteScreen from "./src/screens/PacienteScreen";
+import HomeScreen from "../src/screens/HomeScreen";
+import RegistroScreen from "../src/screens/RegistroScreen";
+import DoctorScreen from "../src/screens/DoctorScreen";
+import PacienteScreen from "../src/screens/PacienteScreen";
 
 const Tab = createBottomTabNavigator();
 
