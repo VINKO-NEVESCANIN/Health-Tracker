46bfb0c9b7c3a8751e55c2cbefd66b68793aff69
JuanAG2405 <jotha142@gmail.com>
2025-10-13 21:41:23 -0700

    Mas Diseño de botones
    Se dejo listo el diseño del boton, asi que te puede tomar para todos los demas

--- frontend/app/doctor/Menu Doctor.tsx
+++ frontend/app/doctor/Menu Doctor.tsx
@@ -1,20 +1,39 @@
 import React from "react";
-import {Image, Pressable, Text, Button, StyleSheet,View} from "react-native";
+import {Image, Pressable, Text, Button, StyleSheet,View, Animated} from "react-native";
 
 export default function MenuDoctor() {
   return (
     <View style={styles.container}>
+      {Array.from({ length: 2 }).map((_, i) => (
       <Pressable style={styles.StylePress} onPress={() => console.log("Logo presionado")}>
-        <Image source={require("../Icon/LoginIcon.png")} style={{ width: 100, height: 100, marginBottom: 20 }} />
-        
+        <Image 
+        source={require("../Icon/LoginIcon.png")} 
+        style={styles.BtnGestionCita}
+        />
+        <Text style={styles.itemText}>Gestion Cita</Text>
       </Pressable>
+      ))}      
+      <Pressable style={styles.StylePress} onPress={() => console.log("Logo presionado")}>
+        <Image 
+        source={require("../Icon/LoginIcon.png")} 
+        style={styles.BtnGestionCita}
+        />
+        <Text style={styles.itemText}>Gestion Cita</Text>
+      </Pressable>
+              
         </View>
+        
+
+    
   );
 }
 
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20, marginTop: 50 },
+  container: { flex: 1, padding: 20, marginTop: 50,  flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 10,
+        padding: 10, },
   title: { fontSize: 20, fontWeight: "bold", marginBottom: 20 },
   input: {
     borderWidth: 1,
@@ -28,10 +47,20 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: "#ddd",
   },
-  itemText: { fontWeight: "bold" },
+  itemText: { 
+    fontWeight: "bold", 
+    marginTop: 97,
+    marginLeft: 20,
+    position: 'absolute',
+    
+  },
 
   BtnGestionCita:{
-
+     width: 100, 
+     height: 100,
+     marginBottom: 20,
+     alignSelf: 'center',
+     borderRadius: 20,
   },
 
   BtnPacientes:{
@@ -47,9 +76,12 @@ const styles = StyleSheet.create({
   },
 
   StylePress:{
-    backgroundColor: "#000000ff",
-    width: 100,
-    height: 100,
-
+    backgroundColor: "#C9B1FF",
+    width: 120,
+    height: 120,
+    boxShadow: "16px 8px 16px rgba(0, 0, 0, 0.25)",
+    borderRadius: 20,
+    border: '2px solid black'
+    
   }
 });
